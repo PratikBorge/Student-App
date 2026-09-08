8175f7c16646e0ea3d179f3027ed95459ac7abda
Pratik <pratikborge37@gmail.com>
2026-09-08 09:37:06 +0530

    update instance

--- TerraformModule/Resources/EKS/main.tf
+++ TerraformModule/Resources/EKS/main.tf
@@ -10,7 +10,7 @@ resource "aws_eks_node_group" "eks_node_group" {
     cluster_name = aws_eks_cluster.eks_cluster.name
     subnet_ids = var.public_subnet_ids
     node_role_arn = "arn:aws:iam::267307198563:role/noderole" //use role created for eks cluster nodegroup
-    instance_types = "c7i-flex.large"
+    instance_types = ["c7i-flex.large"]
     scaling_config {
        desired_size = 1
        max_size     = 2
